ff0109bf8d821ab20dfce4bfc0c2a1f3ae03217a
Ahdai Tsai <ahdai_tsai@htc.com>
2023-05-11 17:17:12 +0800

    Add more debug info

--- lib/ik-renderer/renderer/Renderer.ts
+++ lib/ik-renderer/renderer/Renderer.ts
@@ -49,6 +49,8 @@ export class Renderer {
       this.app.on('update', dt => {
         if (!this.isRunning) return;
 
+        Logger.Instance.log("[Renderer] this.app.on('update'", dt);
+
         this.updateCallbacks.forEach(cb => {
           cb(dt);
         });
